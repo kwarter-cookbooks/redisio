b3d1b067181a21f01ec1c5cb2c2dafe9ee98708f
Armando Guereca <aguereca@gmail.com>
2014-08-05 12:04:24 -0700

    Sync with upstream

--- metadata.rb
+++ metadata.rb
@@ -16,4 +16,4 @@ recipe "redisio::enable", "This recipe is used to start the redis instances and 
 recipe "redisio::disable", "this recipe is used to stop the redis instances and disable them in the default run levels"
 recipe "redisio::redis_gem", "this recipe will install the redis ruby gem into the system ruby"
 
-depends "ulimit", ">= 0.1.3"
+depends "ulimit", ">= 0.1.2"
